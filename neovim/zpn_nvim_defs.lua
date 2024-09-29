c1f11e91abf4ea6af9395fde4314bb6a20e325bb
efunmail <efunmail@gmail.com>
2024-09-30 00:54:03 +0100

    Create zpn_nvim_defs.lua

---
+++ neovim/zpn_nvim_defs.lua
@@ -0,0 +1,52 @@
+local _COMMENT = [[
+-- ** ITERATE thro a table - https://stackoverflow.com/a/17437077
+-- for k, v in pairs(require('lspconfig.util'))
+--   do print (k, v)
+-- end
+
+-- for k, v in pairs(require('lspconfig.util').get_lsp_clients())
+-- -- for k, v in pairs(require('lspconfig.util').get_lsp_clients({name = 'intelephense'}))
+--   do print (k, v)
+-- end
+
+local my_data = {
+  { id = 110, name = 'Ami', },
+  { id = 120, name = 'Bob', },
+}
+my_data = vim.tbl_deep_extend('force', my_data, {
+  { id = 130, name = 'Chas', },
+})
+-- for k, v in pairs(my_data)
+--   do print (k, v.name)
+-- end
+]]
+_COMMENT = nil
+
+local M = {}
+
+function M.hello() print('Hello') end
+
+-- # LSPCONFIG
+M.lspconfig = {
+  -- ## LUA
+  lua_ls = {
+    -- cmd = {'nothing_123'}, -- TEMP:
+    settings = {
+      -- [kickstart]
+      Lua = {
+        completion = {
+          callSnippet = 'Replace',
+        },
+        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        -- diagnostics = { disable = { 'missing-fields' } },
+      },
+    }
+  },
+}
+
+-- # TREESITTER
+M.treesitter = {
+  MSG = 'TODO',
+}
+
+return M
