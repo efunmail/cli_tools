691e0d5e97ebe0e23d0cb941b542777273ca8568
efunmail <efunmail@gmail.com>
2024-10-01 00:32:45 +0100

    Create lua_syntax_main.lua

---
+++ neovim/lua/lua_syntax_main.lua
@@ -0,0 +1,14 @@
+_COMMENT = [[
+  ## Run (using Neovim)
+
+  ~/neovim/0101/bin/nvim -l lua_syntax_main.lua
+]]
+
+MAIN = require('lua_syntax')
+
+print(MAIN.hello())
+print(MAIN.hello('Ami'))
+
+print(MAIN.treesitter.MSG)
+
+print(MAIN.string_match())
